936b0dabc9e7bd4b7bad8f78b79660068039d717
amaraamaebi <jonkbog@gmail.com>
2021-01-03 12:24:48 +0100

    second init

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const sort = (arr) => {
     // const arr = [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
     for(let m = 0 ; m < arr.length; m++){
         for (let n = 1; n < arr.length; n++){
-            if(arr[n] < arr[n-1])
+            if(arr[n] > arr[n-1])
             {
                 var ref = arr[n]; 
                 arr[n] = arr[n - 1]; 
